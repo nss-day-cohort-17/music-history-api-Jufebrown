0b1e1f9a3d2a70db82c03b058ee438ff56460ca6
Jufebrown <jufebrown@gmail.com>
2017-04-19 11:34:18 -0500

    added single song to list of routes on root index

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,18 @@
 // <require express Router and require the songs.js router file>
 const {Router} = require('express')
 const router = Router()
+const songsRouter = require('./songs')
 
 // this is an example of a root document. It returns a json representation of
 // all of our endpoints. (Just one endpoint exists in this tiny example app)
-router.get('/', function(req, res) {
+router.get('/', (req, res, next) => {
   res.json({
     "title": "The Music History API",
     "songs": "http://localhost:3000/api/v1/songs",
+    "songById": "http://localhost:3000/api/v1/songs/:songId"
   });
 });
+
+router.use(songsRouter)
+
+module.exports = router
