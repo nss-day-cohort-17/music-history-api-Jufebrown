b134cbb99bab342dedc105b7425073a816e35c0e
Jufebrown <jufebrown@gmail.com>
2017-04-19 10:22:09 -0500

    required routes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 let app = express();
 
 // <Include the router index file>
+const routes = require('./routes/')
+
 // <Setup your routes middleware>
 // <catch any undefined routes with a 404 middleware>
 // <Handle any errors that occur in the routing with error handlers defined at the bottom of our 
