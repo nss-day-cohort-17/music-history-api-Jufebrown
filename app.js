664c50e50ff258a75bfdf61f8b940d4b6b57b90c
Jufebrown <jufebrown@gmail.com>
2017-04-19 10:23:08 -0500

    called routes and added 404 handler

--- app.js
+++ app.js
@@ -9,5 +9,16 @@ const routes = require('./routes/')
 
 // <Setup your routes middleware>
 // <catch any undefined routes with a 404 middleware>
-// <Handle any errors that occur in the routing with error handlers defined at the bottom of our 
+// <Handle any errors that occur in the routing with error handlers defined at the bottom of our
 // middleware stack>
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/api/v1/', routes)
+
+app.use((req, res, next) => {
+  var err = new Error('File Not Found')
+  err.status = 404
+  next(err)
+})
