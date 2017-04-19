641694d5325688aaa1c1a9069a3fba42609e6829
Jufebrown <jufebrown@gmail.com>
2017-04-19 11:29:00 -0500

    changed from quotes to ticks

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log('Listening on port: ${port}')
+  console.log(`Listening on port: ${port}`)
 })
 
 module.exports = app
