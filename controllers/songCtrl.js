cecbe15bbfd9952b12e56ee26669b6df4ee38f5d
Jufebrown <jufebrown@gmail.com>
2017-04-19 10:01:01 -0500

    added showCtrl methods to get all songs or a single song

--- controllers/songCtrl.js
+++ controllers/songCtrl.js
@@ -5,3 +5,23 @@
 // <stretch goal: methods for adding, deleting, editing a song>
 const {bookshelf} = require('../db/database')
 const Song = require('../models/song')
+
+module.exports.getSong = ({params:{id}}, res, next) => {
+  Song.getSingleSong(id)
+  .then((song) => {
+    res.status(200).json(show)
+  })
+  .catch((error) => {
+    next(error)
+  })
+}
+
+module.exports.getAllShows = (req, res, next) => {
+  Song.getAll()
+  .then((songs) => {
+    res.status(200).json(songs)
+  })
+  .catch((error) => {
+    next(error)
+  })
+}
